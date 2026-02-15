9742fdd51aa337a1444d2fed49f1c7b3a4f98ab3
TobleroneAddict <goober@bigalfccnotice.com>
2026-02-15 13:18:13 -0600

    part clicking

--- src/Editor/VAB.cpp
+++ src/Editor/VAB.cpp
@@ -8,11 +8,18 @@ void VAB::destroy_model() {
 }
 
 int VAB::Start(Bundle* assets, Bundle* parts) {
-    
+    part_tree.clear();
+
+    Part part;
+    part_tree.push_back(part);
+    Part part2;
+    part2.attPos.x += 5;
+    part_tree.push_back(part2);
+
     auto tp_i = touchpad_getinfo();
     tp_h = (float)tp_i->height;
     tp_w = (float)tp_i->width;
-    
+
 
     if (!hide_vab) {
         if (me.load_group(assets,"resources/vab/vab")) return 1;
@@ -69,7 +76,7 @@ void VAB::editor_controls() {
         page_index--;
         page_key_held = true;
     }
-    if (!isKeyPressed(KEY_NSPIRE_F) && !isKeyPressed(KEY_NSPIRE_R)) page_key_held = false; 
+    if (!isKeyPressed(KEY_NSPIRE_F) && !isKeyPressed(KEY_NSPIRE_R)) page_key_held = false;
 
     if (isKeyPressed(KEY_NSPIRE_X) && part_sel_index < 17 && !part_sel_key_held) {
         part_sel_index++;
@@ -79,7 +86,7 @@ void VAB::editor_controls() {
         part_sel_index--;
         part_sel_key_held = true;
     }
-    if (!isKeyPressed(KEY_NSPIRE_Z) && !isKeyPressed(KEY_NSPIRE_X)) part_sel_key_held = false; 
+    if (!isKeyPressed(KEY_NSPIRE_Z) && !isKeyPressed(KEY_NSPIRE_X)) part_sel_key_held = false;
 }
 
 //Actions for mouse click, happens once on click
@@ -91,7 +98,15 @@ void VAB::Update() {
     clock.tick();
     touchpad_scan(&touchpad);
     editor_controls();
-    
+
+    /*
+    printf("P: %d,Cont:%d Prox: %d\n",
+        touchpad.pressed,
+        touchpad.contact,
+        touchpad.proximity
+    );
+    */
+
     //VAB main code
 
     //Click oneshot
@@ -106,24 +121,24 @@ void VAB::Update() {
 }
 
 void VAB::render() {
-    
+
     linalg::vec<float,3> out = cam.wrapper(); //Get rotation
     camera_zoom = linalg::clamp(camera_zoom,-250,-5);
-    
+
     //Limit cam from going thru top of VAB
     float damper = linalg::sin(out.x * 0.01745329);
     float CAM_LIMITER = MAX_CAMERA_HEIGHT + abs(camera_zoom) * damper;
     float CAM_LIMITER_BOTTOM = linalg::max(abs(camera_zoom) * damper,0);
-    
+
     camera_height = linalg::clamp(camera_height,CAM_LIMITER_BOTTOM,CAM_LIMITER);
     cam.camera_controller(Camera::FREE);
-    
+
 
     glColor3f(0.0f, 0.0f, 0.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     //IN CAM
     glPushMatrix();
-    
+
 
     float rp_x = linalg::sin(out.y * 0.01745329);
     float rp_y = linalg::cos(out.y * 0.01745329);
@@ -136,9 +151,9 @@ void VAB::render() {
     nglRotateY(out.y);
 
     glTranslatef(0, -camera_height - 100, 0);
-    
-    
-    
+
+
+
     //VAB
     glPushMatrix();
     glColor3f(0.4f, 0.7f, 1.0f);
@@ -149,7 +164,7 @@ void VAB::render() {
     glScale3f(100, 100, 100);
 
     //VAB OFFSET
-    
+
     bool transformed = false;
     for(ngl_object* obj : full_scene) //Loop through AnGL scene
     {
@@ -161,62 +176,74 @@ void VAB::render() {
     glPopMatrix();//OUT VAB
 
     //Sample part
-    glPushMatrix();
+
 
     //Contact: Touched
     //Pressed: Button pressed
 
-    
-    
-    printf("P: %d,Cont:%d Prox: %d\n",  
-        touchpad.pressed,
-        touchpad.contact,
-        touchpad.proximity
-    );
 
-    //Default origin
-    glTranslatef(0,100,0);
+
+
+
+    
 
     //Touchpad offsets
     if (grabbed_part) {
-    float t_off_x = touchpad.x / tp_w;
-    float t_off_y = touchpad.y / tp_h;
-    t_off_x -= 0.5f;  t_off_y -= 0.5f;
-    
-    
-    float mult = 1.4f * abs(camera_zoom);
-
-    float c_x = t_off_x * mult * rp_y;
-    float c_z = t_off_x * mult * rp_x;
-    float c_y = t_off_y * mult * linalg::cos(out.x * 0.01745329) + camera_height;
+        float t_off_x = touchpad.x / tp_w;
+        float t_off_y = touchpad.y / tp_h;
+        t_off_x -= 0.5f;  t_off_y -= 0.5f;
 
 
-    //Later: fix this for looking down/up
+        float mult = 1.4f * abs(camera_zoom);
+
+        float c_x = t_off_x * mult * rp_y;
+        float c_z = t_off_x * mult * rp_x;
+        float c_y = t_off_y * mult * linalg::cos(out.x * 0.01745329) + camera_height;
 
 
-    glTranslatef(c_x,c_y,c_z);
+        //Later: fix this for looking down/up
+
+
+        part_tree[1].attPos = {c_x,c_y,c_z};
     }
 
-    glScale3f(10,10,10);
+    
 
-    transformed = false;
+
     ngl_object* obj = part_group.get_object("MK1");
-    glBindTexture(obj->texture);
-    nglDrawArray(obj->vertices, obj->count_vertices, obj->positions, obj->count_positions, processed, obj->draw_mode, !transformed);    
+
+    for(Part &p : part_tree) //Loop through AnGL scene
+    {
+        glPushMatrix();
+        //Default origin
+        glTranslatef(0,130,0);
+
+        //Offset by part pos
+        glTranslatef(p.attPos.x,
+            p.attPos.y,
+            p.attPos.z);
+            
+        glScale3f(10,10,10);
+        glBindTexture(obj->texture);
+        nglDrawArray(obj->vertices, obj->count_vertices, obj->positions, obj->count_positions, processed, obj->draw_mode, true);
+
+        glPopMatrix();
+    }
 
     glTranslatef(0,2.0f,0);
     glScale3f(0.4f,0.4f,0.4f);
+    transformed = false;
 
-    glBindTexture(node->texture);
-    nglDrawArray(node->vertices, node->count_vertices, node->positions, node->count_positions, processed, 
-        node->draw_mode, !transformed);    
+    // glBindTexture(node->texture);
+    // nglDrawArray(node->vertices, node->count_vertices, node->positions, node->count_positions, processed,
+    //     node->draw_mode, !transformed);
 
 
 
-    glPopMatrix();
+    
     //End parts
 
-    glPopMatrix();
+    glPopMatrix(); //End Camera
 
     //Show pallete texture at 4x X-res
     if (show_pallete) {
@@ -236,5 +263,5 @@ void VAB::render() {
     }
 
 
-    
+
 }
